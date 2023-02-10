de713b89396da997fe55dcb4fe8e6e9b81a5990a
Valeriia Kraft <airkraft20@gmail.com>
2023-02-10 21:33:22 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int firstBalance = 30 ;
+        int refillBalance = 2000 ;
+        int bonus = refillBalance/ 100 ;
+        int totalBalance = firstBalance + refillBalance ;
+
+        if (refillBalance > 1000){
+            System.out.println(bonus);
+            System.out.println(totalBalance + bonus);
+        } else {
+            System.out.println(totalBalance);
+     }
+   }
+}
