5b8caa4ceab2da1dd80c9ec8261e3cf33d72acf8
Valeriia Kraft <airkraft20@gmail.com>
2023-02-11 22:40:44 +0700

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int firstBalance = 30 ;
-        int refillBalance = 2000 ;
-        int bonus = refillBalance/ 100 ;
-        int totalBalance = firstBalance + refillBalance ;
+        int firstBalance = 30;
+        int refillBalance = 2000;
+        int bonus = refillBalance / 100;
+        int totalBalance = firstBalance + refillBalance;
 
-        if (refillBalance > 1000){
+        if (refillBalance > 1000) {
             System.out.println(bonus);
             System.out.println(totalBalance + bonus);
         } else {
             System.out.println(totalBalance);
-     }
-   }
+        }
+    }
 }
